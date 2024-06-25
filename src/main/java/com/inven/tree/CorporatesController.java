ac5a62bbd16bf154fc60cea4f4c588731e355638
solkim1 <162541398+solkim1@users.noreply.github.com>
2024-06-25 11:02:33 +0900

    Update CorporatesController.java

--- src/main/java/com/inven/tree/CorporatesController.java
+++ src/main/java/com/inven/tree/CorporatesController.java
@@ -3,11 +3,6 @@ package com.inven.tree;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-=======
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,7 +11,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
->>>>>>> de1af2c2ec8d46f767daf91bcf994fd12640878f
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inven.tree.mapper.CorporatesMapper;
@@ -31,10 +25,6 @@ public class CorporatesController {
     private CorporatesMapper corpMapper;
 
     @GetMapping("/corporates")
-<<<<<<< HEAD
-    public List<Corporates> getCorporates() { //회사 리스트
-        return corpMapper.selectAllCorporates();
-=======
     public List<Corporates> getCorporates(@RequestParam("corpIdx") String corpIdx) { //회사 리스트
         return corpMapper.selectAllCorporates(corpIdx);
     }
@@ -47,6 +37,5 @@ public class CorporatesController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to update corporates.");
         }
->>>>>>> de1af2c2ec8d46f767daf91bcf994fd12640878f
     }
 }
